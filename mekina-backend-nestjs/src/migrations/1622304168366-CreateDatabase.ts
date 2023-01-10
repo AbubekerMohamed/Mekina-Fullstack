f3094cf092fb5f52de3ab7f4dc7d9e99aa52f311
AbubekerMohammed <abumoha1892@gmail.com>
2023-01-09 21:07:30 -0900

    database configured

--- mekina-backend-nestjs/src/migrations/1622304168366-CreateDatabase.ts
+++ mekina-backend-nestjs/src/migrations/1622304168366-CreateDatabase.ts
@@ -2,10 +2,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateDatabase1622304168366 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.createDatabase('yourcar', true);
+    queryRunner.createDatabase('mekina', true);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.dropDatabase('yourcar', true);
+    queryRunner.dropDatabase('mekina', true);
   }
 }
